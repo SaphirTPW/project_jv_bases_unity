d766b3fbf176261c5a4dd24ec41bad4a9bae4482
SaphirTPW <nath.bonkena@gmail.com>
2016-10-12 15:43:25 +0200

    fini les movements des persos

---
+++ Versus Fighting Game/Assets/_Scripts/P1CharacterMove.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P1CharacterMove : MonoBehaviour {
+
+    [SerializeField]
+
+    float Speed = 10.0f;
+
+    [SerializeField]
+
+    float jumpSpeed = 5.0f;
+
+    private Rigidbody rb;
+
+    private Vector3 direction;
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       direction = Vector3.zero;
+        //en avant
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction += -Vector3.forward * Time.deltaTime;
+        }
+            
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction = Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction = -Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction = Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+        
+        gameObject.transform.position += direction * Speed * Time.deltaTime;
+
+    }
+}
---
+++ Versus Fighting Game/Assets/_Scripts/P2CharecterMove.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P2CharecterMove : MonoBehaviour {
+    [SerializeField]
+
+    float Speed = 10.0f;
+
+    [SerializeField]
+
+    float jumpSpeed = 5.0f;
+
+    private Rigidbody rb;
+
+    private Vector3 direction;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Vector3.zero;
+        //en avant
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction += -Vector3.forward * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction = Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction = -Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime);
+            direction = Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+
+        gameObject.transform.position += direction * Speed * Time.deltaTime;
+
+    }
+}
