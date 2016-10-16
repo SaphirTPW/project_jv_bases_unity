f05913d634d0a159d13accd2edea9e4fb1b4c096
SaphirTPW <nath.bonkena@gmail.com>
2016-10-16 14:26:06 +0200

    peaufinement du gameplay

--- Versus Fighting Game/Assets/_Scripts/NavMeshMove.cs
+++ Versus Fighting Game/Assets/_Scripts/NavMeshMove.cs
@@ -3,16 +3,41 @@ using System.Collections;
 
 public class NavMeshMove : MonoBehaviour {
 
-    public Transform target;
-    NavMeshAgent agent;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        
+        agent.autoBraking = false;
+
+        GotoNextPoint();
     }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
 
     void Update()
     {
-        agent.SetDestination(target.position);
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (agent.remainingDistance < 0.5f)
+            GotoNextPoint();
     }
 }
---
+++ Versus Fighting Game/Assets/_Scripts/SpawnItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnItem : MonoBehaviour {
+
+    public GameObject itemPrebab;
+    private float Timer;
+
+    void Awake()
+    {
+        Timer = Time.time + 20;
+    }
+    
+    // Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Timer < Time.time)
+        {
+            Instantiate(itemPrebab, transform.position, transform.rotation);
+            Timer = Time.time + 20; 
+        }
+	}
+}
